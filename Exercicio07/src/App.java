f398cc9d7cb1df8985dadc538e6e09b87c0b5b34
Altair Dias <89491054+altairfd@users.noreply.github.com>
2022-06-26 16:56:49 -0300

    Add files via upload

---
+++ Exercicio07/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        FilaDinamica fila = new FilaDinamica();
+        fila.enfileirar(new No("1", null, null));
+        fila.enfileirar(new No("4", null, null));
+        fila.enfileirar(new No("5", null, null));
+        fila.enfileirar(new No("2", null, null));
+        fila.print();
+    }
+}
---
+++ Exercicio07/src/FilaDinamica.java
@@ -0,0 +1,56 @@
+public class FilaDinamica {
+
+    private No cabeca;
+    private No cauda;
+    private int tamanho; 
+
+    public void enfileirar(No novoElemento){
+        if(cabeca == null){
+            cabeca = novoElemento; 
+            cauda = cabeca;
+        }else{
+            novoElemento.setProx(cauda.getProx());
+            novoElemento.setAnt(cauda);
+            cauda.setProx(novoElemento);
+            cauda = novoElemento;
+        }
+        tamanho++;
+    }
+
+    public No desenfileirar(){
+        No aux = cabeca;
+        if(aux == null){
+            return null;
+        }
+
+        cabeca = cabeca.getProx();
+        cabeca.setAnt(null);
+        aux.setAnt(null);
+        aux.setProx(null);
+        tamanho--;
+        return aux;
+    }
+
+    public int getTamanho(){
+        return tamanho;
+    }
+
+    public void print() {
+        No aux = this.cabeca;
+        No fim = null;
+        while (aux != null) {
+            String dado = aux.getDado();
+            System.out.print(dado + "->" );
+            fim = aux;
+            aux = aux.getProx();
+        }
+        System.out.print("FIM\n\n");
+        aux = fim;
+        while (aux != null) {
+            String dado = aux.getDado();
+            System.out.print(dado + "->");
+            aux = aux.getAnt();
+        }
+        System.out.print("Inicio\n\n");
+    }
+}
